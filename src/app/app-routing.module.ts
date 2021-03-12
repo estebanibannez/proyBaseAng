c354e57e4c59c6a9f9104e54c90a037b547d6fc6
Esteban Ibañez <e.ibannez.p@gmail.com>
2021-03-12 02:03:42 -0300

    initial commit base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PagesComponent } from './pages/pages.component';
+import { ProgressComponent } from "./pages/progress/progress.component";
+import { Page404Component } from "./pages/page404/page404.component";
+import { RegisterComponent } from "./auth/register/register.component";
+import { LoginComponent } from "./auth/login/login.component";
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "",
+    component:PagesComponent,
+    children:[
+      { path: "dashboard", component: DashboardComponent },
+      { path: "page404", component: Page404Component },
+      { path: "progress", component: ProgressComponent },
+      { path: "", redirectTo: "/dashboard", pathMatch: "full" }
+    ]
+  },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+
+
+  { path: "**", component: Page404Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { Page404Component } from './pages/page404/page404.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadcumbsComponent } from './shared/breadcumbs/breadcumbs.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { PagesComponent } from 'src/app/pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    Page404Component,
+    DashboardComponent,
+    BreadcumbsComponent,
+    SidebarComponent,
+    HeaderComponent,
+    ProgressComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
